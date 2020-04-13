96615f7af31678c6a1a9ecfd3d12902531ee0849
Wares <fazelyar.wares@gmail.com>
2020-04-13 03:02:47 +0000

    functions chained in waterfall formation

---
+++ index.js
@@ -0,0 +1,54 @@
+// const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('162.245.144.188', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Coords:' , coords);
+// });
+
+
+// const { fetchISSFlyOverTimes } = require('./iss');
+
+// const myCoords = { latitude: '49.26200', longitude: '-123.09230' };
+
+// fetchISSFlyOverTimes(myCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return
+//   }
+
+//   console.log("It worked! Returned ISS info:", passTimes);
+// });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+
+  printPassTimes(passTimes);
+});
