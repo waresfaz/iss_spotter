dd28152d26ead3c314dc0885355f96063de0d699
Wares <fazelyar.wares@gmail.com>
2020-04-13 04:19:04 +0000

    all functions chained and result returned using promise

--- index2.js
+++ index2.js
@@ -1,4 +1,14 @@
-const { fetchMyIP } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(body => console.log(body));
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
